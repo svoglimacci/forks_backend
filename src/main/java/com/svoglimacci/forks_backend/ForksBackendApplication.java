732b4c81241a91c6c4b71e0b3d22cea53de3beb9
svoglimacci <s.voglimacci@tuta.io>
2025-07-23 10:01:23 -0400

    init

---
+++ src/main/java/com/svoglimacci/forks_backend/ForksBackendApplication.java
@@ -0,0 +1,13 @@
+package com.svoglimacci.forks_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForksBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ForksBackendApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/svoglimacci/forks_backend/TestForksBackendApplication.java
@@ -0,0 +1,12 @@
+package com.svoglimacci.forks_backend;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestForksBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.from(ForksBackendApplication::main).with(TestcontainersConfiguration.class)
+        .run(args);
+  }
+
+}
